a559c49c368a1b96b3b5a284a6d4019d321296a0
Nurlybek Nurzhan <136187061+Nurlybek-Nurzhan@users.noreply.github.com>
2024-09-25 08:38:30 +0500

    OHHHHHHHHHHH

--- assets/src/script/swiper-play.js
+++ assets/src/script/swiper-play.js
@@ -1,5 +1,5 @@
-import Swiper from "./swiper/swiper";
-import Autoplay from "./swiper/modules/autoplay.mjs";
+import Swiper from "swiper";
+import { Autoplay } from "swiper/modules";
 
 const play = document.body.querySelector("#play");
 const play_icon = document.body.querySelector(".bx-pause-circle");
